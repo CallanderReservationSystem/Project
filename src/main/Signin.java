aa5ebf291cd9a7470c9cb4cf225867c3e3346b14
hsagga <hsagga@calstatela.edu>
2017-09-29 14:26:45 -0700

    Merged models package into main package.

--- src/main/Signin.java
+++ src/main/Signin.java
@@ -17,8 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import models.MyModel;
-
 @WebServlet("/Signin")
 public class Signin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
