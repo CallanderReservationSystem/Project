71c6ecdfa2f01b74a5cef17ed2b9ce7e68569203
hsagga <hsagga@calstatela.edu>
2017-09-29 11:58:04 -0700

    update .gitignore changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,9 +26,7 @@ public class Main extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
