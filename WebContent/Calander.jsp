c71983a71683fce4c8d2cb72584d85992cfb803c
rdunning75 <ryandunning57@gmail.com>
2017-10-05 23:43:00 -0700

    Added necesary files for calendar to work, fixed calendar day cells to redirect when clicked, and updated git ignore to include .js files

--- WebContent/Calander.jsp
+++ WebContent/Calander.jsp
@@ -5,10 +5,37 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+ 		<link rel="stylesheet" href="fullcalendar.min.css">
+ 		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+ 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+ 		<script src="moment.min.js"></script>
+ 		<script src="fullcalendar.js"></script>
+ 		
+ 		
+		<script>		
+			$(document).ready(function() {
+				
+				$('#calendar').fullCalendar({
+				
+					  navLinks: false
+					  ,dayClick: function(days) { 
+						  window.location.href= "Member.jsp";
+						  
+					    }
+        			});
+ 
+   			 });
+		</script>
+		
+<title>Create your Calendar</title>
 </head>
 <body>
+<div id='calendar'></div>
 
 </body>
 </html>
---
+++ src/main/Calendar.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Calendar
+ */
+@WebServlet("/Calendar")
+public class Calendar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Calendar() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList x = new ArrayList();
+		
+		CalendarEventModel c = new CalendarEventModel();
+		 c.setId(1);
+		 c.setStart("2013-07-28");
+		 c.setEnd("2013-07-29");
+		 c.setTitle("Task in Progress");
+		 
+		 CalendarEventModel d =new CalendarEventModel();
+		 d.setId(2);
+		 d.setStart("2013-07-26");
+		 d.setEnd("2013-08-28");
+		 d.setTitle("Task in Progress");
+		 
+		 x.add(c);
+		 x.add(d);
+		 
+		 request.setAttribute("calendar", d);
+		 response.sendRedirect("Calendar.jsp");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
