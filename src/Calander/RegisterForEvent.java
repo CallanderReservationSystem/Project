7cc7c96914be8264185dd3a983eb6e815f9155db
hsagga <hsagga@calstatela.edu>
2017-10-31 17:19:11 -0700

    rearange files

--- src/Calander/RegisterForEvent.java
+++ src/Calander/RegisterForEvent.java
@@ -9,27 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 
-/**
- * Servlet implementation class RegisterForEvent
- */   	
 @WebServlet("/RegisterForEvent")
 public class RegisterForEvent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterForEvent() {
-        super();
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Connection c = null;
-		
+
 		Integer id = null;
 		Integer userId = null;
 		String eventName = null;
@@ -38,24 +25,21 @@ public class RegisterForEvent extends HttpServlet {
 		String location = null;
 		String start = null;
 		String end = null;
-		
+
 		String url = "jdbc:mysql://cs3.calstatela.edu/cs3337stu03";
 		String SQLuser = "cs3337stu03";
 		String SQLpass = "K!c7YAg.";
-		String sql = "select * from events where cid = " + calId; // event is placeholder name of the event that the user wants to register for
-		
-		try
-		{
+		String sql = "select * from events where cid = " + calId;
+
+		try {
 			c = DriverManager.getConnection(url, SQLuser, SQLpass);
 			Statement st = c.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			
-			while (rs.next())
-			{
+
+			while (rs.next()) {
 				id = rs.getInt("id");
 				userId = rs.getInt("uid");
 				eventName = rs.getString("title");
-				//calId = rs.getInt("cid");
 				location = rs.getString("location");
 				start = rs.getString("start");
 				end = rs.getString("end");
@@ -65,52 +49,13 @@ public class RegisterForEvent extends HttpServlet {
 			request.setAttribute("start", start);
 			request.setAttribute("end", end);
 
-			request.getRequestDispatcher("RegisterForEvent.jsp").forward(request, response);
-		}
-		catch (SQLException e)
-		{
+			request.getRequestDispatcher("/Calander/RegisterForEvent.jsp").forward(request, response);
+		} catch (SQLException e) {
 			throw new ServletException(e);
 		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		Connection c = null;
-//		
-//		Integer id = null;
-//		Integer userId = null;
-//		String eventName = null;
-//		Integer calId = null;
-//		
-//		String url = "jdbc:mysql://cs3.calstatela.edu/cs3337stu03";
-//		String SQLuser = "cs3337stu03";
-//		String SQLpass = "K!c7YAg.";
-//		String sql = "select * from events"; // event is placeholder name of the event that the user wants to register for
-//		
-//		try
-//		{
-//			c = DriverManager.getConnection(url, SQLuser, SQLpass);
-//			Statement st = c.createStatement();
-//			ResultSet rs = st.executeQuery(sql);
-//			
-//			while (rs.next())
-//			{
-//				id = rs.getInt("id");
-//				userId = rs.getInt("uid");
-//				eventName = rs.getString("title");
-//				calId = rs.getInt("cid");
-//				
-//				
-//			}
-//			request.setAttribute("id", id);
-//
-//			request.getRequestDispatcher("RegisterForEvent.jsp").forward(request, response);
-//		}
-//		catch (SQLException e)
-//		{
-//			throw new ServletException(e);
-//		}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 }
